930b19f7b52ce579f4b212ca93dfb54407f2ad60
Xavier Fernandes <xavierbatistafernandes.001@gmail.com>
2025-11-24 19:57:49 +0000

    🧤 add accordions for projects

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,11 +24,11 @@ export default async function RootLayout({
     <html lang="en" className={`${theme}`}>
       <head>
         <title>Have you met Xavier?</title>
-        <link rel="icon" type="image/x-icon" href="/favicon.svg" />
+        <link rel="icon" type="image/x-icon" href="/favicon.png" />
       </head>
 
       <body
-        className={`${font.className} mx-auto flex max-h-dvh max-w-[768px] flex-col bg-amber-50 text-neutral-900 md:border-x-[0.5px] dark:bg-zinc-900 dark:text-amber-50 md:dark:border-amber-50`}
+        className={`${font.className} mx-auto flex max-h-dvh max-w-3xl flex-col bg-amber-50 text-neutral-900 md:border-x-[0.5px] dark:bg-zinc-900 dark:text-amber-50 md:dark:border-amber-50`}
       >
         <AppContextProvider>
           <header className="relative min-h-20 border-b-[0.5px]">
@@ -64,47 +64,9 @@ export default async function RootLayout({
                 </li>
                 <hr className="border-t-[0.5px] border-[#4f4f4f]" />
               </ul>
-              {/* <section className="flex flex-col items-center gap-10">
-                <p className="mt-3 leading-relaxed font-light">
-                  <span className="font-bold">
-                    Hey there! Glad to see you've wandered this far across the
-                    web!
-                  </span>{' '}
-                  Welcome to my little digital corner.
-                </p>
-
-                <img
-                  src="/images/cat.webp"
-                  alt="An animated cat drinking a cup of coffee."
-                  className="w-1/2 max-w-[250px] select-none"
-                />
-                <div>
-                  <p className="text-xs">
-                    I'm {new Date().getFullYear() - 2000} years old. I'm a
-                    coffee lover. They do say that developers are machines that
-                    turn coffee into code. That's me: I have thousands of new
-                    ideas in my mind that I would like to implement. Some of
-                    them actually come to life, and I invite you to have a look
-                    at them!
-                  </p>
-                 
-                </div>
-              </section> */}
             </div>
             <div>{children}</div>
           </main>
-          {/* <footer className="min-h-20 border-t-[0.5px]">
-            This is the end of the page. Thank you for scrolling all the way.
-            <hr className="border-t-[0.5px] border-[#4f4f4f]" />
-            <a
-              href="mailto:fernandesbatistaxavier@gmail.com"
-              className="flex items-center gap-2 p-3 text-xs transition-colors duration-200 ease-out hover:bg-white/10"
-            >
-              <MessagesIcon className="h-4 w-4 fill-black dark:fill-white" />
-              <span>Reach out!</span>
-            </a>
-            <hr className="border-t-[0.5px] border-[#4f4f4f]" />
-          </footer> */}
         </AppContextProvider>
       </body>
     </html>
--- src/components/organisms/sections/projects.tsx
+++ src/components/organisms/sections/projects.tsx
@@ -1,20 +1,105 @@
 'use client';
 
-import { ArrowLoadingIcon } from '@/src/components/atoms/icons/arrow-loading.icon';
-import { BoxArrowInIcon } from '@/src/components/atoms/icons/box-arrow-in.icon';
 import { HammerIcon } from '@/src/components/atoms/icons/hammer.icon';
-import { InformationIcon } from '@/src/components/atoms/icons/information.icon';
-import { PaperClipIcon } from '@/src/components/atoms/icons/paper-clip.icon';
-import { useDragScroll } from '@/src/hooks/use-drag-scroll';
+import { Accordion } from '../../atoms/accordion';
 
 export default function Projects() {
+  const projects = [
+    {
+      id: 'sproutalis',
+      url: 'https://sproutalis.vercel.app',
+      header: (
+        <h2 className="my-1 text-lg text-[#5b5b5b] dark:text-amber-50/75">
+          Sproutalis - <span className="text-sm">2026</span>
+        </h2>
+      ),
+      content: (
+        <div>
+          <a
+            className="block text-sm font-bold text-amber-600 dark:text-amber-200"
+            href="https://sproutalis.vercel.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            https://sproutalis.vercel.app
+          </a>
+          <p className="mt-4">
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+            quae ab illo inventore veritatis et quasi architecto beatae vitae
+            dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+            eos qui ratione voluptatem sequi nesciunt.
+          </p>
+        </div>
+      ),
+    },
+    {
+      id: 'terras-lusas',
+      url: 'https://terras-lusas.vercel.app',
+      header: (
+        <h2 className="my-1 text-lg text-[#5b5b5b] dark:text-amber-50/75">
+          Terras Lusas - <span className="text-sm">2025</span>
+        </h2>
+      ),
+      content: (
+        <div>
+          <a
+            className="block text-sm font-bold text-amber-600 dark:text-amber-200"
+            href="https://terras-lusas.vercel.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            https://terras-lusas.vercel.app
+          </a>
+          <p className="mt-4">
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+            quae ab illo inventore veritatis et quasi architecto beatae vitae
+            dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+            eos qui ratione voluptatem sequi nesciunt.
+          </p>
+        </div>
+      ),
+    },
+
+    {
+      id: 'rubiks-rush',
+      url: 'https://rubiks-rush.vercel.app',
+      header: (
+        <h2 className="my-1 text-lg text-[#5b5b5b] dark:text-amber-50/75">
+          Rubik's Rush - <span className="text-sm">2024</span>
+        </h2>
+      ),
+      content: (
+        <div>
+          <a
+            className="block text-sm font-bold text-amber-600 dark:text-amber-200"
+            href="https://rubiks-rush.vercel.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            https://rubiks-rush.vercel.app
+          </a>
+          <p className="mt-4">
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+            quae ab illo inventore veritatis et quasi architecto beatae vitae
+            dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+            eos qui ratione voluptatem sequi nesciunt.
+          </p>
+        </div>
+      ),
+    },
+  ];
+
   return (
     <div className="h-full max-w-screen overflow-y-scroll py-10">
       <div className="flex h-10 items-center gap-2">
         <HammerIcon className="h-full fill-black dark:fill-white" />
-        <h1 className="text-[min(4vw,2rem)] font-bold">
-          WHAT I HAVE BEEN UP TO.
-        </h1>
+        <h1 className="text-[min(4vw,2rem)]">WHAT I HAVE BEEN UP TO.</h1>
       </div>
       <p className="mt-5">
         I'm a coffee lover. They do say that developers are machines that turn
@@ -22,159 +107,57 @@ export default function Projects() {
         that I would like to implement. Some of them actually come to life, and
         I invite you to have a look at them!
       </p>
-      <div className="my-8 flex flex-col items-start gap-4">
-        <div className="flex h-48 items-start gap-10">
-          <div className="flex h-full flex-col gap-2">
-            <div className="ml-2 flex cursor-pointer items-center gap-2 text-2xl">
-              <InformationIcon className="size-6 fill-amber-50" />
-              <span>2025</span>
-            </div>
-            <div className="ml-5 w-px grow bg-amber-100"></div>
-          </div>
-          <div>
-            <p className="cursor-pointer">Terras Lusas</p>
-            <p className="cursor-pointer">Lunch Tram</p>
-            <p className="cursor-pointer">Blog</p>
-          </div>
-        </div>
-
-        <div className="flex h-48 items-start gap-10">
-          <div className="flex h-full flex-col gap-2">
-            <div className="cursor-pointer text-2xl">2024</div>
-            <div className="ml-5 w-px grow bg-amber-100"></div>
-          </div>
-          <div>
-            <p className="cursor-pointer">Rubik's Rush</p>
-            <p className="cursor-pointer">Lunch Tram</p>
-          </div>
-        </div>
-        <div className="flex h-48 items-start gap-10">
-          <div className="flex h-full flex-col gap-2">
-            <div className="cursor-pointer text-2xl">College</div>
-            <div className="ml-5 w-px grow bg-amber-100"></div>
-          </div>
-          <div>
-            <p className="cursor-pointer">Rubik's Rush</p>
-            <p className="cursor-pointer">Lunch Tram</p>
-          </div>
-        </div>
+      <div
+        className="mt-10 flex w-full flex-col gap-2 overflow-auto"
+        style={{ scrollbarWidth: 'none' }}
+      >
+        {projects.map(({ id, header, content }) => (
+          <>
+            <Accordion key={id} header={header} content={content}></Accordion>
+            <hr className="h-[0.5px] bg-amber-300" />
+          </>
+        ))}
       </div>
     </div>
   );
 }
 
-function Timeline() {
-  const timelineRef = useDragScroll<HTMLDivElement>(1.5);
-
-  return (
-    <div
-      ref={timelineRef}
-      className="w-full overflow-x-scroll overflow-y-hidden select-none active:cursor-grabbing"
-      style={{ scrollbarWidth: 'none' }}
-    >
-      <div className="relative h-[50vh] w-[300%] border-blue-500">
-        <div
-          id="top-section"
-          className="relative flex h-1/2 w-full flex-row-reverse"
-        >
-          <div id="2025" className="relative basis-1/3 border-red-600">
-            <div className="absolute bottom-0 flex w-full flex-col items-center justify-center border-pink-400">
-              <a
-                aria-label="Link to Terras Lusas's GitHub page."
-                href="https://github.com/xavier-batista-fernandes/terras-lusas"
-              >
-                <PaperClipIcon className="h-10 stroke-white p-1" />
-              </a>
-              <h2 className="text-4xl font-bold">2025</h2>
-            </div>
-          </div>
-          <div id="2024" className="relative basis-1/3 border-red-600">
-            <div className="absolute bottom-0 flex w-full flex-col items-center justify-center border-pink-400">
-              <a
-                aria-label="Link to Blog's GitHub page."
-                href="https://github.com/xavier-batista-fernandes/blog-x"
-              >
-                <PaperClipIcon className="h-10 stroke-white p-1" />
-              </a>
-              <h2 className="text-4xl font-bold">2024</h2>
-            </div>
-          </div>
-          <div id="2023" className="relative basis-1/3 border-red-600">
-            <div className="absolute bottom-0 flex w-full flex-col items-center justify-center border-pink-400">
-              <a
-                aria-label="Link to IPMA+'s GitHub page."
-                href="https://github.com/xavier-batista-fernandes/ipma-mais"
-              >
-                <PaperClipIcon className="h-10 stroke-white p-1" />
-              </a>
-              <h2 className="text-4xl font-bold">2023</h2>
-            </div>
-          </div>
-        </div>
-
-        <div
-          id="timeline"
-          className="relative h-0 w-full border-t-1 border-dashed bg-transparent"
-        >
-          <BoxArrowInIcon className="animateSway absolute -left-7 hidden h-6 -translate-y-1/2 stroke-white md:block"></BoxArrowInIcon>
-          <p className="absolute bottom-1 left-4 hidden text-sm md:block">
-            One day I had an idea.
-          </p>
-          <div className="absolute left-0 aspect-square h-2 -translate-y-1/2 rounded-full bg-white"></div>
-          <p className="absolute top-1 left-4 hidden text-sm md:block">
-            ... So I started coding.
-          </p>
-
-          <ArrowLoadingIcon className="animateRotate absolute -right-8 hidden h-6 -translate-y-1/2 stroke-white md:block"></ArrowLoadingIcon>
-          <p className="absolute right-4 bottom-1 hidden text-sm md:block">
-            Now I'm here!
-          </p>
-          <div className="absolute right-0 aspect-square h-2 -translate-y-1/2 rounded-full bg-white"></div>
-          <p className="absolute top-1 right-4 hidden text-sm md:block">
-            ... Still coding to this day.
-          </p>
-        </div>
-
-        <div
-          id="bottom-section"
-          className="relative flex h-1/2 w-full flex-row-reverse"
-        >
-          <div id="2025-d" className="h-full w-1/3">
-            <div className="mx-auto flex w-2/3 flex-col justify-center gap-1 overflow-hidden">
-              <div className="h-2/5">
-                <h3 className="text-1xl font-bold">Terras Lusas</h3>
-                <p className="">
-                  A web app to explore the beautiful landscapes of Portugal.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div id="2024-d" className="h-full w-1/3">
-            <div className="mx-auto flex w-2/3 flex-col gap-1">
-              <div className="h-2/5">
-                <ul>
-                  <li>
-                    <h3 className="text-1xl font-bold">Blog</h3>
-                    <p className="">
-                      A platform to share my thoughts and experiences.
-                    </p>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div id="2023-d" className="h-full w-1/3">
-            <div className="mx-auto flex w-2/3 flex-col justify-center gap-1">
-              <div className="h-2/5">
-                <h3 className="text-1xl font-bold">IPMA+</h3>
-                <p className="">
-                  A project to automate weather data collection.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
+{
+  /* <div className="my-8 flex flex-col items-start gap-4">
+  <div className="flex h-48 items-start gap-10">
+    <div className="flex h-full flex-col gap-2">
+      <div className="ml-2 flex cursor-pointer items-center gap-2 text-2xl">
+        <InformationIcon className="size-6 fill-amber-50" />
+        <span>2025</span>
       </div>
+      <div className="ml-5 w-px grow bg-amber-100"></div>
     </div>
-  );
+    <div>
+      <p className="cursor-pointer">Terras Lusas</p>
+      <p className="cursor-pointer">Lunch Tram</p>
+      <p className="cursor-pointer">Blog</p>
+    </div>
+  </div>
+
+  <div className="flex h-48 items-start gap-10">
+    <div className="flex h-full flex-col gap-2">
+      <div className="cursor-pointer text-2xl">2024</div>
+      <div className="ml-5 w-px grow bg-amber-100"></div>
+    </div>
+    <div>
+      <p className="cursor-pointer">Rubik's Rush</p>
+      <p className="cursor-pointer">Lunch Tram</p>
+    </div>
+  </div>
+  <div className="flex h-48 items-start gap-10">
+    <div className="flex h-full flex-col gap-2">
+      <div className="cursor-pointer text-2xl">College</div>
+      <div className="ml-5 w-px grow bg-amber-100"></div>
+    </div>
+    <div>
+      <p className="cursor-pointer">Rubik's Rush</p>
+      <p className="cursor-pointer">Lunch Tram</p>
+    </div>
+  </div>
+</div>; */
 }
